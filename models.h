adde7995c351d1f3db8ab3c6764c299a4f01e744
Sarjano <rahul.mssr@gmail.com>
2025-12-18 00:04:17 +0530

    Create models.h

---
+++ models.h
@@ -0,0 +1,11 @@
+#ifndef MODELS_H
+#define MODELS_H
+
+void exponential_growth_solver(void);
+void logistic_growth_solver(void);
+void RL_solver(void);
+void Newton_cooling_solver(void);
+void LCR_current_finder(void);
+void Mass_spring_finder(void);
+
+#endif
